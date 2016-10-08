fc602371f1646b4f2361d515edf08c65d34d314e
Nicolas Polhamus <nicolas.polhamus@gmail.com>
2016-10-08 18:44:40 -0500

    Create base.component.ts

---
+++ app/base.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-app',
+    templateUrl: 'base.component.html'
+})
+export class BaseComponent { }
