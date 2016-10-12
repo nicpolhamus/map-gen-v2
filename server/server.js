d1be0837f4bf958f8dae70e84c7e6187fbcf4054
Nicolas Polhamus <nicolas.polhamus@gmail.com>
2016-10-12 18:19:24 -0500

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,38 @@
+(function () {
+    'use strict';
+
+    var express = require('express');
+    var winston = require('winston');
+    var expressWinston = require('express-winston');
+    var app = express();
+    var path = require('path');
+    var rootPath = path.normalize(__dirname + '/../');
+
+    app.use(expressWinston.logger({
+        transports: [
+            new winston.transports.Console({
+                json: false,
+                colorize: true
+            })
+        ],
+        meta: false,
+        msg: "HTTP {{req.method}} {{req.url}}",
+        expressFormat: true,
+        colorStatus: true,
+        ignoreRoute: function (req, res) {
+            return false;
+        }
+    }));
+
+    // app.use(express.static(rootPath + '/src'));
+    // app.use(express.static(rootPath + '/node_modules'));
+    app.use(express.static(rootPath));
+
+    app.get('*', function (req, res) {
+        res.send(rootPath + 'index.html');
+    });
+
+    app.listen(process.env.port || 3000, function () {
+        (process.env.port) ? console.log('Listening on port ' + process.env.port) : console.log('Listening on port 3000');
+    });
+})();
