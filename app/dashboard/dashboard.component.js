ba6019c104cda7c2a8aa854f58a54ae15a7e68f4
BagelTheKid <nicolas.polhamus@gmail.com>
2016-10-15 01:01:04 -0500

    Adds input validation

--- app/dashboard/dashboard.component.js
+++ app/dashboard/dashboard.component.js
@@ -1,3 +1,4 @@
+/// <reference path="../../typings/globals/jquery/index.d.ts" />
 "use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -9,6 +10,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+require('bootstrap');
 var map_1 = require('../shared/map');
 var DashboardComponent = (function () {
     function DashboardComponent() {
@@ -18,9 +20,43 @@ var DashboardComponent = (function () {
         this.rooms = 0;
     }
     DashboardComponent.prototype.generateMap = function (l, w, r) {
-        var map = new map_1.Map(Number.parseInt(w), Number.parseInt(l), Number.parseInt(r));
-        map.createMap();
-        this.mapString = map.toString();
+        if (this.validateInput(l, w, r)) {
+            var map = new map_1.Map(Number.parseInt(w), Number.parseInt(l), Number.parseInt(r));
+            map.createMap();
+            this.mapString = map.toString();
+        }
+        else {
+            this.mapString = '';
+        }
+    };
+    DashboardComponent.prototype.validateInput = function (l, w, r) {
+        l = Number.parseInt(l);
+        w = Number.parseInt(w);
+        r = Number.parseInt(r);
+        var valid = false;
+        this.validationText = '';
+        if (l > 0 && w > 0 && r > 0) {
+            if (l > 200) {
+                this.validationText += 'Length needs to be less than 200!' + '\n';
+            }
+            if (w > 200) {
+                this.validationText += 'Width needs to be less than 200!' + '\n';
+            }
+            if (r > ((l + w) / 2)) {
+                this.validationText += 'Rooms need to be less than Length + Width divided by two!' + '\n';
+            }
+            else {
+                valid = true;
+            }
+            if (!valid) {
+                $('#validation-modal').modal('show');
+            }
+        }
+        else {
+            this.validationText = 'Length, Width, and Rooms need to be greater than 0!';
+            $('#validation-modal').modal('show');
+        }
+        return valid;
     };
     DashboardComponent = __decorate([
         core_1.Component({
--- app/dashboard/dashboard.component.ts
+++ app/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
+/// <reference path="../../typings/globals/jquery/index.d.ts" />
+
+
 import { Component } from '@angular/core';
+
+import 'bootstrap';
 
 import { Map } from '../shared/map';
 import { Direction } from '../shared/direction';
@@ -17,10 +22,41 @@ export class DashboardComponent {
     width: number = 0;
     length: number = 0;
     rooms: number = 0;
+    validationText: string;
     constructor() {}
     generateMap(l, w, r): void {
-        let map = new Map(Number.parseInt(w),Number.parseInt(l),Number.parseInt(r));
-        map.createMap();
-        this.mapString = map.toString();
+        if (this.validateInput(l,w,r)) {
+            let map = new Map(Number.parseInt(w),Number.parseInt(l),Number.parseInt(r));
+            map.createMap();
+            this.mapString = map.toString();
+        } else {
+            this.mapString = '';
+        }
+    }
+    private validateInput(l, w, r): boolean {
+        l = Number.parseInt(l);
+        w = Number.parseInt(w);
+        r = Number.parseInt(r);
+        let valid: boolean = false;
+        this.validationText = '';
+        if (l > 0 && w > 0 && r > 0) {
+            if (l > 200) {
+                this.validationText += 'Length needs to be less than 200!' + '\n';
+            } if (w > 200) {
+                this.validationText += 'Width needs to be less than 200!' + '\n';
+            } if (r > ((l+w)/2)) {
+                this.validationText += 'Rooms need to be less than Length + Width divided by two!' + '\n';
+            } else {
+                valid = true;
+            }
+
+            if (!valid) {
+                $('#validation-modal').modal('show');
+            }
+        } else {
+            this.validationText = 'Length, Width, and Rooms need to be greater than 0!';
+            $('#validation-modal').modal('show');
+        }
+        return valid;
     }
 }
